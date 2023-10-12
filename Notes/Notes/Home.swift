e0a1555a8c41a8edcd99c227803c96633cab7e0b
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-12 15:54:27 -0400

    Add Basic Layout
    with NavigationView and List

--- Notes/Notes/Home.swift
+++ Notes/Notes/Home.swift
@@ -9,6 +9,22 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
+        
+        NavigationView {
+            List(0..<9) { i in
+                Text("We are at \(i)")
+                    .padding()
+            }
+            .navigationTitle("Notes")
+            .navigationBarItems(trailing: Button(action:
+                                                    {print("Add a note")},
+                                                 label:
+                                                    {Text("Add")}
+                                                )
+            )
+        }
+        
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
