1320626e3de7a00ff960646a86636d57c4cd275a
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-12 13:21:02 -0400

    Complete RESTful APIs

--- notes-api/src/index.js
+++ notes-api/src/index.js
@@ -6,15 +6,9 @@ const Note = require('./models/notes')
 const app = express();
 app.use(express.json());
 
-app.get('/notes', (req, res) => {
-    fs.readFile(`${__dirname}/notes.json`, 'utf-8', (err, data) => {
-        if(err) {
-            return console.log(err);
-        }
-        res.send(data);
-    })
-})
+// CRUD
 
+// CREATE
 app.post('/notes', async (req, res) => {
     const note = new Note(req.body);
     try {
@@ -22,6 +16,44 @@ app.post('/notes', async (req, res) => {
         res.status(200).send(note);
     } catch (error) {
         res.status(400).send(error);
+    }
+})
+
+// READ
+app.get('/notes', async (req, res) => {
+    try {
+        const notes = await Note.find({}) // fetch notes in db
+        res.status(200).send(notes);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+// UPDATE
+app.patch('/notes/:id', async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id);
+        if(!note) {
+            return res.status(404).send("The note does not exist!");
+        }
+        note.note = req.body.note;
+        await note.save();
+        res.status(200).send(note);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
+
+// DELETE
+app.delete('/notes/:id', async (req, res) => {
+    try {
+        const note = await Note.findByIdAndDelete(req.params.id);
+        if(!note) {
+            return res.status(404).send("The note does not exist!");
+        }
+        res.status(200).send("The note has been deleted!");
+    } catch (error) {
+        res.status(500).send(error);
     }
 })
 
