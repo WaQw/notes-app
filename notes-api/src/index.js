f449204804da9259e276df910a6380a6042c8ac4
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-12 12:49:05 -0400

    Add model and db

--- notes-api/src/index.js
+++ notes-api/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+require('./db/mongoose')
+const Note = require('./models/notes')
 
 const app = express();
+app.use(express.json());
 
 app.get('/notes', (req, res) => {
     fs.readFile(`${__dirname}/notes.json`, 'utf-8', (err, data) => {
@@ -9,6 +12,15 @@ app.get('/notes', (req, res) => {
             return console.log(err);
         }
         res.send(data);
+    })
+})
+
+app.post('/notes', (req, res) => {
+    const note = new Note(req.body);
+    note.save().then(() => {
+        res.status(200).send(note);
+    }).catch((err) => {
+        res.status(500).send(err);
     })
 })
 
