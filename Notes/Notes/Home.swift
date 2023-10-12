6c166a8e4f4a5ac302f8f6b6ace12e9240a82276
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-12 16:59:04 -0400

    Fetch Notes

--- Notes/Notes/Home.swift
+++ Notes/Notes/Home.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var notes = [Note]() // update the UI everytime notes changes
+    
     var body: some View {
         
         NavigationView {
-            List(0..<9) { i in
-                Text("We are at \(i)")
+            List(self.notes) { el in
+                Text(el.note)
                     .padding()
             }
+            .onAppear(perform: {
+                fetchNotes()
+            })
             .navigationTitle("Notes")
             .navigationBarItems(trailing: Button(action:
                                                     {print("Add a note")},
@@ -33,6 +39,29 @@ struct Home: View {
         }
         .padding()
     }
+    
+    func fetchNotes() {
+        let url = URL(string: "http://localhost:3000/notes")!
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { data, res, err in
+            guard let data = data else { return } // if there is data, then goto next line
+            do {
+                let notes = try JSONDecoder().decode([Note].self, from: data)
+                self.notes = notes
+                print("Hi \(notes)")
+            } catch {
+                print(error)
+            }
+        })
+        
+        task.resume()
+    }
+}
+
+struct Note: Identifiable, Codable {
+    var id: String { _id }
+    var _id: String
+    var note: String
 }
 
 struct ContentView_Previews: PreviewProvider {
