6cbf7464a471fb53137c6044b0ec341d3a042e21
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-13 14:18:06 -0400

    Add Delete Function
    with Alert and LongPressGesture

--- Notes/Notes/Home.swift
+++ Notes/Notes/Home.swift
@@ -10,7 +10,14 @@ import SwiftUI
 struct Home: View {
     
     @State var notes = [Note]() // update the UI everytime notes changes
+    
     @State var showAdd = false
+    
+    @State var showAlert = false
+    @State var deleteItem: Note?
+    var alert: Alert {
+        Alert(title: Text("Delete"), message: Text("Are you sure you want to delete this note"), primaryButton: .destructive(Text("Delete"), action: deleteNote), secondaryButton: .cancel())
+    }
     
     var body: some View {
         
@@ -18,7 +25,14 @@ struct Home: View {
             List(self.notes) { el in
                 Text(el.note)
                     .padding()
+                    .onLongPressGesture(perform: {
+                        self.showAlert.toggle()
+                        deleteItem = el
+                    })
             }
+            .alert(isPresented: $showAlert, content: {
+                alert
+            })
             .sheet(isPresented: $showAdd, onDismiss: fetchNotes, content: {
                 AddNoteView()
             })
@@ -52,13 +66,34 @@ struct Home: View {
             do {
                 let notes = try JSONDecoder().decode([Note].self, from: data)
                 self.notes = notes
-                print("Hi \(notes)")
             } catch {
                 print(error)
             }
         })
         
         task.resume()
+    }
+    
+    func deleteNote() {
+        guard let id = deleteItem?._id else { return }
+        let url = URL(string: "http://localhost:3000/notes/\(id)")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, res, err in
+            guard err == nil else { return }
+            guard let data = data else { return }
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                }
+            } catch let error {
+                print(error)
+            }
+        })
+        task.resume()
+        fetchNotes()
     }
 }
 
